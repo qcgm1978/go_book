7213c5dfca5d1fe96d93ed489274d3f6fbb09a30
qcgm197874@gmail.com <qcgm197874@gmail.com>
2021-12-21 12:52:21 +0800

    1

---
+++ quantecon-mini-example-master/mini_book/_build/jupyter_execute/docs/Part3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+# ## Part 3.Greater than the sum of its parts
+# 
+# At this point, you’ve learned a number of AI techniques that draw from classical tree search, machine learning, and reinforcement learning. Each is powerful on its own, but each also has limitations. To make a truly strong Go AI, you’ll need to combine everything you’ve learned so far. Integrating all these pieces is a serious engineering feat. This part covers the architecture of AlphaGo, the AI that rocked the Go world—and the AI world! To conclude the book, you’ll learn about the elegantly simple design of AlphaGo Zero, the strongest version of AlphaGo to date.
